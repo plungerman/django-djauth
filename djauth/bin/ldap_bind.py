c6014047542f386d77a3e0cdae30ed48b2ee2440
Bender <plungerman@gmail.com>
2014-09-18 10:07:51 -0500

    new shell script to test ldap auth for a user

---
+++ djauth/bin/ldap_bind.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.contrib.auth.models import User, Group
+
+from djauth.LDAPManager import LDAPManager
+
+#username = "lsemler"
+username = "skirk"
+password = "Carthage1847"
+
+# initialise the LDAP manager
+l = LDAPManager()
+
+result_data = l.search(username,field="cn")
+
+if not result_data:
+    print result_data
+    print "fail"
+else:
+    print result_data
+    print "\n\n"
+    print "dn = %s" % result_data[0][0]
+    print "\n\n"
+    print "Attempt to bind to the user's DN."
+    print "\n\n"
+    l.bind(result_data[0][0],password)
