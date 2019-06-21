09229ea40ba2398cfaee0ac0b6d8794963fbbd20
plungerman <plungerman@gmail.com>
2019-06-21 14:30:54 -0500

    removed old code for django versions before 1.6

--- djauth/views.py
+++ djauth/views.py
@@ -21,15 +21,7 @@ def loggedout(request, template_name='accounts/logged_out.html'):
         from django.contrib.auth.models import AnonymousUser
         request.user = AnonymousUser()
 
-    # VERSION returns (1, x, x, u'final', 1)
-    # hopefully, we will be done using django 1.6 by the time 2.x comes out
-    if django.VERSION[1] > 6:
-        from django.shortcuts import render
-        response = render(request, template_name)
-    else:
-        from django.shortcuts import render_to_response
-        response = render_to_response(
-            template_name, context_instance=RequestContext(request)
-        )
+    from django.shortcuts import render
+    response = render(request, template_name)
 
     return response
