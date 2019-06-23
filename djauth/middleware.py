fc1ba25331b14f0ba10cb15682ec3a5b094e4042
plungerman <plungerman@gmail.com>
2019-06-23 15:16:14 -0500

    revert back to original

--- djauth/middleware.py
+++ djauth/middleware.py
@@ -5,16 +5,21 @@ from django.http import HttpResponseForbidden
 
 class AutomaticUserLoginMiddleware(MiddlewareMixin):
 
+    #def process_request(self, request):
     def process_view(self, request, view_func, view_args, view_kwargs):
-        if not AutomaticUserLoginMiddleware._is_user_authenticated(request):
+        #if not AutomaticUserLoginMiddleware._is_user_authenticated(request):
+        if not request.user.is_authenticated:
             user = auth.authenticate(request)
             if user is None:
                 return HttpResponseForbidden()
 
             request.user = user
             auth.login(request, user)
+        return request
 
     @staticmethod
     def _is_user_authenticated(request):
         user = request.user
-return user and user.is_authenticated
+        #return user and user.is_authenticated
+        return user.is_authenticated
+
