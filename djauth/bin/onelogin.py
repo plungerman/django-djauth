daf1f95715688535cb13d405a93cca4a1a014cb0
plungerman <plungerman@gmail.com>
2021-01-29 15:17:22 -0600

    check for returned data before proceeding

--- djauth/bin/onelogin.py
+++ djauth/bin/onelogin.py
@@ -57,24 +57,31 @@ def main():
         print("El Dap init")
         result_data = eldap.search(find=username, field='cn')
         print(result_data)
-        cid = result_data[0][1][settings.LDAP_ID_ATTR][0]
-        print('cid = {0}'.format(cid))
-        if password:
-            print("attempting to bind...")
-            try:
-                bind = eldap.bind(result_data[0][0], password)
-            except Exception as auth_fail:
-                bind = auth_fail
-            print(bind)
-        print('groups:\n\n')
-        groups = []
-        for role in result_data[0][1][ldap_group_attr]:
-            if isinstance(role, bytes):
-                role = role.decode(encoding='utf-8')
-            group = ldap_groups.get(role.split(',')[0].split(' ')[0][3:])
-            if group and group not in groups:
-                groups.append(group)
-        print(groups)
+        if result_data:
+            cid = result_data[0][1][settings.LDAP_ID_ATTR][0]
+            print('cid = {0}'.format(cid))
+            if password:
+                print("attempting to bind...")
+                try:
+                    bind = eldap.bind(result_data[0][0], password)
+                except Exception as auth_fail:
+                    bind = auth_fail
+                print(bind)
+            print('groups:\n\n')
+            groups = []
+            for role in result_data[0][1][ldap_group_attr]:
+                if isinstance(role, bytes):
+                    role = role.decode(encoding='utf-8')
+                group = ldap_groups.get(role.split(',')[0].split(' ')[0][3:])
+                if group and group not in groups:
+                    groups.append(group)
+            print(groups)
+            # testing for djbeca
+            luser = result_data[0][1]
+            print('username={0}'.format(luser['cn'][0]))
+            print('email={0}'.format(luser['mail'][0]))
+            print('first_name={0}'.format(luser['givenName'][0]))
+            print('last_name={0}'.format(luser['sn'][0]))
     else:
         print("El Dap fail")
 
